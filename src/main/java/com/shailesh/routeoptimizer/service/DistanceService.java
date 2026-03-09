a0f480b8188645318fba2c0d3c74fe6b279ae4fa
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2026-03-09 11:05:25 +0530

    Implemented Haversine formula for real world distance calculation

--- src/main/java/com/shailesh/routeoptimizer/service/DistanceService.java
+++ src/main/java/com/shailesh/routeoptimizer/service/DistanceService.java
@@ -8,9 +8,16 @@ public class DistanceService {
     public double calculateDistance(double lat1, double lon1,
                                     double lat2, double lon2) {
 
-        double x = lat2 - lat1;
-        double y = lon2 - lon1;
+        final int EARTH_RADIUS = 6371; // Radius of earth in KM
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2)
+                * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        return Math.sqrt(x * x + y * y);
+        return EARTH_RADIUS * c; // Distance in KM
     }
 }
