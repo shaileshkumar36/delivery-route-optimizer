d7b6d667c638b9e200565836dd5f87a26d718a29
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2026-03-10 12:09:54 +0530

    fix datasource config

--- src/main/java/com/shailesh/routeoptimizer/RouteoptimizerApplication.java
+++ src/main/java/com/shailesh/routeoptimizer/RouteoptimizerApplication.java
@@ -3,7 +3,7 @@ package com.shailesh.routeoptimizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class RouteoptimizerApplication {
 
     public static void main(String[] args) {
