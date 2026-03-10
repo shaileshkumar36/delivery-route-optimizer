cc089fd4367c710b8146cfed04fb18f955ac4bb1
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2026-03-10 12:04:57 +0530

    fix datasource exclusion

--- src/main/java/com/shailesh/routeoptimizer/RouteoptimizerApplication.java
+++ src/main/java/com/shailesh/routeoptimizer/RouteoptimizerApplication.java
@@ -3,10 +3,11 @@ package com.shailesh.routeoptimizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class)
 public class RouteoptimizerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RouteoptimizerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RouteoptimizerApplication.class, args);
+    }
+
 }
